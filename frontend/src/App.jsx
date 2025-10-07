618f64dc8f0ecb8a467cb389918f5209243141ce
jattin Raval <jatinraval14@gmail.com>
2025-10-07 16:06:18 +0530

    installing shadcn UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import{ Button } from "./components/ui/button"
 
 const App = () => {
   return (
+    <div>
     <h1 className="text-3xl font-bold underline text-red-500">Hello World!</h1>
+  <button>Click me</button>
+    </div>
   )
 }
 
